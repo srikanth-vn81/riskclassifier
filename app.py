0ebd4a200cb93012f427d0700e8bed60b23e933b
srikanth-vn81 <68362977+srikanth-vn81@users.noreply.github.com>
2022-11-20 22:38:28 +0530

    Update app.py

--- app.py
+++ app.py
@@ -27,4 +27,4 @@ else:
 excl_merged.to_excel(r'C:\Users\srikanthve\Desktop\Style Reco Output\stylerecoappend.xlsx')
 df1 = pd.read_excel(r"C:\Users\srikanthve\Desktop\Style Reco Output\stylerecoappend.xlsx",skiprows=5)
 df1 = df1.iloc[: , 3:]
-
+df1.to_excel(r'C:\Users\srikanthve\Desktop\Style Reco Output\stylerecoappend_final.xlsx')
