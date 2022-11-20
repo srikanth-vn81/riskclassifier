67fe6a32a0085d4763a8f26ffe83ed843613d424
srikanth-vn81 <68362977+srikanth-vn81@users.noreply.github.com>
2022-11-20 22:18:17 +0530

    Update app.py

--- app.py
+++ app.py
@@ -14,7 +14,6 @@ for file in file_list:
 excl_merged = pd.DataFrame()
 
 for excl_file in excl_list:
-
 	excl_merged = excl_merged.append(excl_file, ignore_index=True)
 
 else:
@@ -24,22 +23,4 @@ else:
                 """
 
        )
-
-excl_merged.to_excel(r'C:\Users\srikanthve\Desktop\Style Reco Output\stylerecoappend.xlsx')
-
-df1 = pd.read_excel(r"C:\Users\srikanthve\Desktop\Style Reco Output\stylerecoappend.xlsx",skiprows=5)
-	
-excl_merged = excl_merged.iloc[: , 3:]
-
-#excl_merged[]
-
-df1=pd.read_excel(excl_merged)
-
-df1.to_excel(r'C:\Users\srikanthve\Desktop\Style Reco Output\stylerecoappend_final.xlsx')
-
-df2 = pd.read_excel(r"C:\Users\srikanthve\Desktop\Style Reco Output\stylerecoappend_final.xlsx")
-
-grouper12 = excl_merged.groupby(['Item Group','Item Code','Proc Group','Description','Colour Size'])
-
-stylereco = grouper12['Tot Req','Order Qty','GRN Qty','PUT Qty','REC in Qty','REC out Qty','MO Issue Quantity','RO Issue Quantity','Balance Qty','Balance Value'].sum().reset_index()
 
