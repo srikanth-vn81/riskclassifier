c93073c4ff651096ae207f6262ba24be9316c6a8
srikanth-vn81 <68362977+srikanth-vn81@users.noreply.github.com>
2022-11-26 12:58:47 +0530

    Update app.py

--- app.py
+++ app.py
@@ -9,20 +9,14 @@ for file in file_list:
 	excl_list.append(pd.read_excel(file))
 excl_merged = pd.DataFrame()
 
+excl_merged[]
 for excl_file in excl_list:
 	excl_merged = excl_merged.append(excl_file, ignore_index=True)
 
-else:
-	st.info(
-            f"""
-                👆 Upload a .xlsx file)
-                """
-
-       )
-
+	
 excl_merged = excl_merged.iloc[: , 3:]
 
-#excl_merged[]
+
 
 df1=pd.read_excel(excl_merged)
 
