1b66e6f496a9118ae7777091c84b1c5090d67224
srikanth-vn81 <68362977+srikanth-vn81@users.noreply.github.com>
2022-11-26 12:17:22 +0530

    Update app.py

--- app.py
+++ app.py
@@ -25,11 +25,11 @@ else:
 
        )
 excl_merged.to_excel(r'C:\Users\srikanthve\Desktop\Style Reco Output\stylerecoappend.xlsx')
-excl_merged = excl_merged.iloc[: , 3:]
 df1 = pd.read_excel(r"C:\Users\srikanthve\Desktop\Style Reco Output\stylerecoappend.xlsx",skiprows=5)
-st.dataframe(df1)
-df1.to_excel(r'C:\Users\srikanthve\Desktop\Style Reco Output\stylerecoappend_final.xlsx')
-st.write(df1)
+df2 = df1.iloc[: , 3:]
+st.dataframe(df2)
+df2.to_excel(r'C:\Users\srikanthve\Desktop\Style Reco Output\stylerecoappend_final.xlsx')
+
 
 #df2 = pd.read_excel(r"C:\Users\srikanthve\Desktop\Style Reco Output\stylerecoappend_final.xlsx")
 
