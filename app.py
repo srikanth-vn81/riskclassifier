89e88f13c911695d644fb3d72e45b6e16c8a4a11
srikanth-vn81 <68362977+srikanth-vn81@users.noreply.github.com>
2022-11-26 13:35:13 +0530

    Update app.py

--- app.py
+++ app.py
@@ -5,3 +5,16 @@ uploaded_file = st.file_uploader(
         key="1",
         help="To activate 'wide mode', go to the hamburger menu > Settings > turn on 'wide mode'",
     )
+if uploaded_file is not None:
+        file_container = st.expander("Check your uploaded .csv")
+        shows = pd.read_csv(uploaded_file)
+        uploaded_file.seek(0)
+        file_container.write(shows)
+else:
+        st.info(
+            f"""
+                👆 Upload a .csv file first. Sample to try: [biostats.csv](https://people.sc.fsu.edu/~jburkardt/data/csv/biostats.csv)
+                """
+        )
+
+        st.stop()
