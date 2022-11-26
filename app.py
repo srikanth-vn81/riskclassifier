38f97e8b1cca24fa5fba5de4f91cfbd9b30635d6
srikanth-vn81 <68362977+srikanth-vn81@users.noreply.github.com>
2022-11-26 13:16:18 +0530

    Update app.py

--- app.py
+++ app.py
@@ -1,11 +1,2 @@
 uploaded_files = st.file_uploader("Please choose a CSV file", accept_multiple_files=True)
 
-for file in uploaded_files:
-
-    bytes_data = file.read()
-
-    st.write("File uploaded:", file.name)
-
-    st.write(bytes_data)
-
-
