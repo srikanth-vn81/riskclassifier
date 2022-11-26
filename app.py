dc0b359c688c6a54eff2aea76863dbe3b4a1672a
srikanth-vn81 <68362977+srikanth-vn81@users.noreply.github.com>
2022-11-26 13:41:43 +0530

    Update app.py

--- app.py
+++ app.py
@@ -1,7 +1,6 @@
 import streamlit as st
 import pandas as pd
-uploaded_file = st.file_uploader(
-        "Choose Stylereco file",accept_multiple_files=True)
+uploaded_file = st.file_uploader("Choose Stylereco file",accept_multiple_files=True)
 if uploaded_file is not None:
         #file_container = st.expander("Check your uploaded .excel")
         shows = pd.read_excel(uploaded_file)
