2305e207ed452bbc908d55d1872ddaf8475b27c9
srikanth-vn81 <68362977+srikanth-vn81@users.noreply.github.com>
2022-11-26 13:20:52 +0530

    Update app.py

--- app.py
+++ app.py
@@ -3,6 +3,6 @@ import pandas as pd
 
 uploaded_files = st.file_uploader("Please choose a Excel file", accept_multiple_files=True)
 for file in uploaded_files:
-  df=file.append(pd.read_excel(file))
+  df=pd.read_excel(file))
   st.write(df)
 
