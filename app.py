462fec2abcd272d7ad714177899b578c677ceeca
srikanth-vn81 <68362977+srikanth-vn81@users.noreply.github.com>
2022-11-26 13:31:50 +0530

    Update app.py

--- app.py
+++ app.py
@@ -1,19 +1,18 @@
 import streamlit as st
 import pandas as pd
 
-    uploaded_file = st.file_uploader(
+uploaded_file = st.file_uploader(
         "",accept_multiple_files=True
         key="1",
         help="To activate 'wide mode', go to the hamburger menu > Settings > turn on 'wide mode'",
     )
 
-    if uploaded_file is not None:
+if uploaded_file is not None:
         file_container = st.expander("Check your uploaded .csv")
         shows = pd.read_csv(uploaded_file)
         uploaded_file.seek(0)
         file_container.write(shows)
-
-    else:
+else:
         st.info(
             f"""
                 👆 Upload a .csv file first. Sample to try: [biostats.csv](https://people.sc.fsu.edu/~jburkardt/data/csv/biostats.csv)
